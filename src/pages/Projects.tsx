5053e45453e43f1db189afd7f29d89d2206dc4ed
Matheus Souza Santos Leite <matheusssleite@outlook.com>
2024-08-01 18:50:55 -0300

    Adicionando menu de design dentro do menu skills

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,94 +1,74 @@
-import myBooks from '../assets/myBooks.png'
-import ultraflix from '../assets/ultraflix.png'
-import myHouse from '../assets/myHouse.png'
+import { useState } from 'react';
 
-import ReactDOM from 'react-dom';
+import myBooks from '../assets/myBooks.png';
+import ultraflix from '../assets/ultraflix.png';
+import myHouse from '../assets/myHouse.png';
 
+import cfc from '../assets/cfc.png';
+import cfc_logo from '../assets/cfc_logo.png';
+import palace from '../assets/palace.png';
+import palace_logo from '../assets/palace_logo.png';
 
-import cfc from '../assets/cfc.png'
-import cfc_logo from '../assets/cfc_logo.png'
-import palace from '../assets/palace.png'
-import palace_logo from '../assets/palace_logo.png'
-import { Design } from '../components/Design'
+interface ProjectProps {
+    imgSrc: string;
+    title: string;
+    link: string;
+}
 
+const Project = function ({ imgSrc, title, link }: ProjectProps) {
+    return (
+        <div className="col-one">
+            <img src={imgSrc} alt={`${title} - Project`} />
+            <b className='text-projects'>{title}</b>
+            <a href={link} className='link-projects' target="_blank" rel="noopener noreferrer">{link}</a>
+        </div>
+    );
+};
 
+const FrontEndProjects = () => (
+    <div className="FrontEnd">
+        <div className='col-one'>
+            <Project imgSrc={myBooks} title="MyBooks" link="https://theus03.github.io/my-books/" />
+        </div>
+        <div className="col-two">
+            <Project imgSrc={ultraflix} title="UltraFlix" link="https://theus03.github.io/ultraflix/" />
+        </div>
+        <div className="col-three">
+            <Project imgSrc={myHouse} title="MyHouse" link="https://theus03.github.io/myHouse/" />
+        </div>
+    </div>
+);
+
+const UIDesignProjects = () => (
+    <div className="UIDesign">
+        <div className="col-one">
+            <img src={cfc} alt="CFC - Project" className='img-project'/>
+            <img src={cfc_logo} alt="CFC - Logo" className='logo-project' />
+        </div>
+        <div className="col-two">
+            <img src={palace} alt="Palace - Project" className='img-project' />
+            <img src={palace_logo} alt="Palace - Logo" className='logo-project' />
+        </div>
+    </div>
+);
 
 export function Projects() {
-    let frontEndHTML = `
-        <div className="FrontEnd">
-            <div className="col-one" style="display: flex; flex-direction: column;">
-                <img src=${myBooks} alt="MyBooks - Project" />
-                <b className='text-projects' style="font: bold 1.3rem Roboto, 'sans-serif';">MyBooks</b>
-                <a href="https://theus03.github.io/my-books/" className='link-projects' target="_blank">https://theus03.github.io/my-books/</a>
-            </div>
-            <div className="col-two" style="display: flex; flex-direction: column;">
-                <img src=${ultraflix} alt="UltraFlix - Project" />
-                <b className='text-projects' font: bold 1.3rem Roboto, 'sans-serif';>UltraFlix</b>
-                <a href="https://theus03.github.io/ultraflix/" className='link-projects' target="_blank">https://theus03.github.io/ultraflix/</a>
-            </div>
-            <div className="col-three" style="display: flex; flex-direction: column;">
-                <img src=${myHouse} alt="MyHouse - Project" />
-                <b className='text-projects' font: bold 1.3rem Roboto, 'sans-serif';>MyHouse</b>
-                <a href="https://theus03.github.io/myHouse/"  className='link-projects' target="_blank">https://theus03.github.io/myHouse/</a>
-            </div>
-        </div>
-    `
+    const [showFrontEnd, setShowFrontEnd] = useState(true);
 
-    let designHTML = `
-        <div className="UIDesign">
-            <div className="col-one" style="display: flex; flex-direction: column;">
-                <img src=${cfc} alt="CFC - Project" className='img-project'/>
-                <img src=${cfc_logo} alt="CFC - Logo" className='logo-project' />
-            </div>
-            <div className="col-two" style="display: flex; flex-direction: column;">
-                <img src=${palace} alt="Palace - Project" className='img-project' />
-                <img src=${palace_logo} alt="Palace - Logo" className='logo-project' />
-            </div>
-        </div>
-    `
-
-    function showBodyProjects(showFrontEnd: boolean, showDesign: boolean) {
-        let bodyProjectsDiv = document.getElementById('body-projects');
-        if (bodyProjectsDiv) {
-            bodyProjectsDiv.innerHTML = frontEndHTML;
-            if (showFrontEnd) {
-                bodyProjectsDiv.innerHTML = frontEndHTML;
-            } else if (showDesign) {
-                bodyProjectsDiv.innerHTML = designHTML;
-            }
-        }
-    }
-
-    return(
+    return (
         <div id="projects">
             <div className="container">
                 <div className="header-projects">
-                        <h1 className="title-projects">Projects</h1>
-                        <div className="choose-projects">
-                            <a className="textChoose-projects" onClick={() => showBodyProjects(true, false)} >Front-End</a>
-                            <a className="textChoose-projects" onClick={() => showBodyProjects(false, true)} >UI Design</a>
-                        </div>
+                    <h1 className="title-projects">Projects</h1>
+                    <div className="choose-projects">
+                        <a className="textChoose-projects" onClick={() => setShowFrontEnd(true)}>Front-End</a>
+                        <a className="textChoose-projects" onClick={() => setShowFrontEnd(false)}>UI Design</a>
                     </div>
+                </div>
                 <div className="body-projects" id="body-projects">
-                    <div className="FrontEnd">
-                        <div className="col-one">
-                            <img src={myBooks} alt="MyBooks - Project" />
-                            <b className='text-projects'>MyBooks</b>
-                            <a href="https://theus03.github.io/my-books/" className='link-projects' target="_blank">https://theus03.github.io/my-books/</a>
-                        </div>
-                        <div className="col-two">
-                            <img src={ultraflix} alt="UltraFlix - Project" />
-                            <b className='text-projects'>UltraFlix</b>
-                            <a href="https://theus03.github.io/ultraflix/" className='link-projects' target="_blank">https://theus03.github.io/ultraflix/</a>
-                        </div>
-                        <div className="col-three">
-                            <img src={myHouse} alt="MyHouse - Project" />
-                            <b className='text-projects'>MyHouse</b>
-                            <a href="https://theus03.github.io/myHouse/"  className='link-projects' target="_blank">https://theus03.github.io/myHouse/</a>
-                        </div>
-                    </div>
+                    {showFrontEnd ? <FrontEndProjects /> : <UIDesignProjects />}
                 </div>
             </div>
         </div>
-    )
+    );
 }
